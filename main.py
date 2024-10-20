05cf7314f02a8000d221b70e17ff31bc7d07491d
dandreu7 <157640055+dandreu7@users.noreply.github.com>
2024-10-19 23:26:31 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI
 from typing import Union
+from pydantic import BaseModel
 
 app = FastAPI()
 
-
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
 @app.get("/")
 async def root():
     return {"Hello": "World"}
@@ -12,3 +16,7 @@ async def root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_id": item_id, "item_name": item.name}
